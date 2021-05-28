43294d3a18c1c841ceee8b3d7f2005e36cc0d7e4
zyt-yt <71649187+zyt-yt@users.noreply.github.com>
2021-05-28 09:20:29 +0800

    Add files via upload

---
+++ LAB3/Code/main.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"interim.h"
+
+extern FILE* yyin;
+extern int yylineno;
+extern int yyparse();
+extern int yyrestart(FILE*);
+extern struct Node* head;
+extern int is_error;
+int yylex();
+
+
+
+int main(int argc,char** argv){
+    if(argc<=1)
+        return 1;
+    FILE* f=fopen(argv[1],"r");
+    if(!f)
+    {
+        perror(argv[1]);
+        return 1;
+    }
+    FILE* f2=fopen(argv[2],"w+");
+    if(!f2)
+    {
+        perror(argv[2]);
+        return 1;
+    }
+    yylineno=1;
+    yyrestart(f);
+    yyparse();
+    //printf("head: %u\n",head);
+    if(is_error==0){
+        //print_tree(head);
+        Program(head);
+        translate_Program(head,f2);
+        }
+    return 0;
+}
+
+//lexic中不能使用注释，会错误
+//syntax中可以使用注释
+
+/*TODO:
+注释！1.3
+
+*/
